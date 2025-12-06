becb05f79e6bcba833752a1cf53c3c88b721ebc0
Ivan <sir239@yandex.ru>
2025-12-06 13:17:15 +0300

    feat(db): set up initial database configuration and create users table
    - Configure SQLAlchemy async engine in alembic.ini
    - Set up Alembic environment for async migrations
    - Create initial users table migration
    - Define User model in user.py

---
+++ models/user.py
@@ -0,0 +1,12 @@
+from sqlalchemy.orm import Mapped, mapped_column
+
+from app.db import Base
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    username: Mapped[str] = mapped_column(unique=True)
+    email: Mapped[str] = mapped_column(unique=True)
+    password: Mapped[str] = mapped_column()
