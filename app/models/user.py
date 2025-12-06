e04d5dc7332227b077e1784ca183c49a754df775
Ivan <sir239@yandex.ru>
2025-12-06 17:58:57 +0300

    refactor(models): Add Project and Task models and update User relationships
    - Add Project and Task models to the models package
    - Update User model to include relationships with Project and Task
    - Modify __init__.py to import new models

--- app/models/user.py
+++ app/models/user.py
@@ -1,12 +1,16 @@
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from app.db import Base
 
 
 class User(Base):
     __tablename__ = "users"
+    __allow_unmapped__ = True
 
     id: Mapped[int] = mapped_column(primary_key=True)
     username: Mapped[str] = mapped_column(unique=True)
     email: Mapped[str] = mapped_column(unique=True)
     password: Mapped[str] = mapped_column()
+
+    projects: Mapped[list["Project"]] = relationship(back_populates="owner")
+    tasks: Mapped[list["Task"]] = relationship(back_populates="assigned_to")
