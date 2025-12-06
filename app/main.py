4773e0fc2db42082c3e4eed5813ba6326a62477b
Ivan <sir239@yandex.ru>
2025-12-06 17:22:53 +0300

    fix: correct import path for auth_router
    - Updated the import statement for auth_router from 'auth.router' to 'app.auth.router'
    - This resolves the ImportError issue when running the application

--- app/main.py
+++ app/main.py
@@ -5,7 +5,7 @@ from app.db import get_db
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.api.v1.user import user_router
-from auth.router import auth_router
+from app.auth.router import auth_router
 
 app = FastAPI(title="TODO")
 app.include_router(user_router)
